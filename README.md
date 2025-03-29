29520358225192421bc60dfd4fcf25356f0edbd5
Jordan Collins <jordan.collins.dev@gmail.com>
2025-03-29 15:39:15 +1100

    feat: init project with basic layout

---
+++ README.md
@@ -0,0 +1,12 @@
+# Jordan Collins - Shorthand Project
+
+## Installation
+
+This project was bootstrapped with Next.js. Simply run:
+
+```bash
+pnpm i && pnpm run dev
+```
+
+and open [http://localhost:3000](http://localhost:3000).
+
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div className="container grid grid-rows-[20px_1fr] items-center justify-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-600">
+      <header className="flex gap-6 flex-wrap items-center justify-center">
+        <button>
+          Change theme
+        </button>
+      </header>
+      <main className="flex flex-col gap-8 row-start-2 items-center bg-amber-800 h-[200px] w-full rounded-lg p-4">
+        <div className="grid grid-rows-[30px_1fr] gap-4 w-full h-full">
+          <div className="bg-amber-100">
+            Progress bar
+          </div>
+          <div className=" bg-amber-300">
+            Sign up form
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
